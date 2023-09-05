a990a6349dd6d6d21d0c342a08576048dd11e9cf
shaunatgeorgian <shaun@georgian.io>
2023-09-04 21:09:11 -0400

    update with tempfile to address file type mismatch error

--- Chat with CSV project/app.py
+++ Chat with CSV project/app.py
@@ -1,26 +1,35 @@
+import pandas as pd
+import tempfile
 import streamlit as st
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
+load_dotenv()
+
+# ... (the rest of your imports and code)
+
 def main():
-
-    load_dotenv()
-
-    st.set_page_config(page_title="Ask your CSV 📊")
-    st.header("Ask your CSV 📊")
+    # ... (your existing code)
 
     user_csv = st.file_uploader("Upload your CSV file", type="csv")
 
     if user_csv is not None:
+        # Read the uploaded file into a DataFrame
+        df = pd.read_csv(user_csv)
+
+        # Save the DataFrame to a temporary file
+        tfile = tempfile.NamedTemporaryFile(delete=False) 
+        tfile.write(df.to_csv(index=False).encode('utf-8'))
+
         user_question = st.text_input("Ask a question about your CSV file:")
 
         llm = OpenAI(temperature=0)
-        agent = create_csv_agent(llm, user_csv, verbose=True)
+        
+        # Pass the temporary file's path to create_csv_agent()
+        agent = create_csv_agent(llm, tfile.name, verbose=True)
 
-        if user_question is not None and user_question.question != "":
-            response = agent.run(user_question)
-            
+        if user_question is not None and user_question != "":
             st.write(f"Your question was: {user_question}")
 
 if __name__ == "__main__":
