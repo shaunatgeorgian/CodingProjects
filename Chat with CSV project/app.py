0fd8a8a1f1471a05341708afe7c084ef833ccda0
shaunatgeorgian <shaun@georgian.io>
2023-09-05 10:49:44 -0400

    update to api key

--- Chat with CSV project/app.py
+++ Chat with CSV project/app.py
@@ -4,13 +4,20 @@ import streamlit as st
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
-
-load_dotenv()
-
-# ... (the rest of your imports and code)
+import os
 
 def main():
-    # ... (your existing code)
+    load_dotenv()
+
+    # Load the OpenAI API key from the environment variable
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV 📈")
 
     user_csv = st.file_uploader("Upload your CSV file", type="csv")
 
@@ -30,7 +37,15 @@ def main():
         agent = create_csv_agent(llm, tfile.name, verbose=True)
 
         if user_question is not None and user_question != "":
-            st.write(f"Your question was: {user_question}")
+            with st.spinner(text="In progress..."):
+                st.write(f"Your question was: {user_question}")
+                try:
+                    # This is where we ask the question and get the answer
+                    response = agent.run(user_question)
+                except Exception as e:
+                    response = f"An error occurred: {e}"
+
+                st.write(f"Answer: {response}")
 
 if __name__ == "__main__":
     main()
