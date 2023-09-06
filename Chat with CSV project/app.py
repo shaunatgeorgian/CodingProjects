e7ce45037e5d72bc8b6871d89db66c761b1c8ba3
shaunatgeorgian <shaun@georgian.io>
2023-09-06 12:47:40 -0400

    update to code

--- Chat with CSV project/app.py
+++ Chat with CSV project/app.py
@@ -1,66 +1,3 @@
-# import pandas as pd
-# import tempfile
-# import streamlit as st
-# from langchain.agents import create_csv_agent
-# from langchain.llms import OpenAI
-# from dotenv import load_dotenv
-# import os
-
-# def main():
-#     load_dotenv()
-
-#     # Load the OpenAI API key from the environment variable
-#     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-#         print("OPENAI_API_KEY is not set")
-#         exit(1)
-#     else:
-#         print("OPENAI_API_KEY is set")
-
-#     st.set_page_config(page_title="Ask your CSV")
-#     st.header("Ask your CSV 📈")
-
-#     # Initialize session state for questions and answers
-#     if 'questions' not in st.session_state:
-#         st.session_state.questions = []
-#     if 'answers' not in st.session_state:
-#         st.session_state.answers = []
-
-#     user_csv = st.file_uploader("Upload your CSV file", type="csv")
-
-#     if user_csv is not None:
-#         # Read the uploaded file into a DataFrame
-#         df = pd.read_csv(user_csv)
-
-#         # Save the DataFrame to a temporary file
-#         tfile = tempfile.NamedTemporaryFile(delete=False) 
-#         tfile.write(df.to_csv(index=False).encode('utf-8'))
-
-
-#         llm = OpenAI(temperature=0)
-        
-#         # Pass the temporary file's path to create_csv_agent()
-#         agent = create_csv_agent(llm, tfile.name, verbose=True)
-
-#         user_question = st.text_input("Ask a question about your CSV file:")
-
-
-#         if user_question:
-#             with st.spinner(text="In progress..."):
-#                 try:
-#                     response = agent.run(user_question)
-#                     st.session_state.questions.append(user_question)
-#                     st.session_state.answers.append(response)
-#                 except Exception as e:
-#                     response = f"An error occurred: {e}"
-#                     st.session_state.answers.append(response)
-            
-#             for q, a in zip(st.session_state.questions, st.session_state.answers):
-#                 st.write(f"Q: {q}")
-#                 st.write(f"A: {a}")
-
-# if __name__ == "__main__":
-#     main()
-
 import pandas as pd
 import tempfile
 import streamlit as st
@@ -72,7 +9,6 @@ import os
 def main():
     load_dotenv()
 
-    # Load the OpenAI API key from the environment variable
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
@@ -85,29 +21,26 @@ def main():
     user_csv = st.file_uploader("Upload your CSV file", type="csv")
 
     if user_csv is not None:
-        # Read the uploaded file into a DataFrame
         df = pd.read_csv(user_csv)
+        
+        # Limiting the DataFrame to first 100 rows to avoid token limit
+        df_limited = df.head(100)
 
-        # Save the DataFrame to a temporary file
-        tfile = tempfile.NamedTemporaryFile(delete=False) 
-        tfile.write(df.to_csv(index=False).encode('utf-8'))
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(df_limited.to_csv(index=False).encode('utf-8'))
 
         user_question = st.text_input("Ask a question about your CSV file:")
 
         llm = OpenAI(temperature=0)
-        
-        # Pass the temporary file's path to create_csv_agent()
         agent = create_csv_agent(llm, tfile.name, verbose=True)
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(f"Your question was: {user_question}")
                 try:
-                    # This is where we ask the question and get the answer
                     response = agent.run(user_question)
                 except Exception as e:
-                    response = f"An error occurred: {e}"
-
+                    response = f"An error occurred: {e}. Try using a shorter question or smaller dataset."
+                    
                 st.write(f"Answer: {response}")
 
 if __name__ == "__main__":
