403ec19f9d90be7518710575827676a3bea08834
shaunatgeorgian <shaun@georgian.io>
2023-09-11 21:03:37 -0400

    new commit

--- Chat with CSV project/app.py
+++ Chat with CSV project/app.py
@@ -1,47 +1,74 @@
-import pandas as pd
-import tempfile
 import streamlit as st
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
 
-def main():
-    load_dotenv()
+# Initialize environment variables
+load_dotenv()
 
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+# Check and store OpenAI API key
+if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+    print("OPENAI_API_KEY is not set")
+    exit(1)
+else:
+    print("OPENAI_API_KEY is set")
 
-    st.set_page_config(page_title="Ask your CSV")
-    st.header("Ask your CSV 📈")
+# Initialize session state variables if they don't exist
+if 'df' not in st.session_state:
+    st.session_state.df = None
 
-    user_csv = st.file_uploader("Upload your CSV file", type="csv")
+if 'prompt_history' not in st.session_state:
+    st.session_state.prompt_history = []
 
-    if user_csv is not None:
-        df = pd.read_csv(user_csv)
-        
-        # Limiting the DataFrame to first 100 rows to avoid token limit
-        df_limited = df.head(100)
+# Store API token in session state
+st.session_state.openai_key = os.getenv("OPENAI_API_KEY")
 
-        tfile = tempfile.NamedTemporaryFile(delete=False)
-        tfile.write(df_limited.to_csv(index=False).encode('utf-8'))
+# Streamlit Interface
+st.title("pandas-ai streamlit interface")
+st.write("A demo interface for [PandasAI](https://github.com/gventuri/pandas-ai)")
+st.write("Looking for an example *.csv-file?, check [here](https://gist.github.com/netj/8836201).")
 
-        user_question = st.text_input("Ask a question about your CSV file:")
+# File Uploader
+if st.session_state.df is None:
+    uploaded_file = st.file_uploader("Choose a CSV file. This should be in long format (one datapoint per row).", type="csv")
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file)
+            st.session_state.df = df
+        except Exception as e:
+            st.error(f"Error reading CSV: {e}")
 
-        llm = OpenAI(temperature=0)
-        agent = create_csv_agent(llm, tfile.name, verbose=True)
+# Form for Question
+with st.form("Question"):
+    question = st.text_input("Question", value="", type="default")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        try:
+            with st.spinner():
+                llm = OpenAI(api_token=st.session_state.openai_key)
+                pandas_ai = PandasAI(llm)
+                x = pandas_ai.run(st.session_state.df, prompt=question)
+                
+                fig = plt.gcf()
+                if fig.get_axes():
+                    st.pyplot(fig)
+                st.write(x)
+                st.session_state.prompt_history.append(question)
+        except Exception as e:
+            st.error(f"Error processing question: {e}")
 
-        if user_question is not None and user_question != "":
-            with st.spinner(text="In progress..."):
-                try:
-                    response = agent.run(user_question)
-                except Exception as e:
-                    response = f"An error occurred: {e}. Try using a shorter question or smaller dataset."
-                    
-                st.write(f"Answer: {response}")
+# Display current DataFrame and prompt history
+if st.session_state.df is not None:
+    st.subheader("Current dataframe:")
+    st.write(st.session_state.df)
 
-if __name__ == "__main__":
-    main()
+st.subheader("Prompt history:")
+st.write(st.session_state.prompt_history)
+
+# Clear button
+if st.button("Clear"):
+    st.session_state.prompt_history = []
+    st.session_state.df = None
