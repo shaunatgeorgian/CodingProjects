cbf3c57b24badf7ca6b2629a5a37ef340bde7ae2
shaunatgeorgian <shaun@georgian.io>
2023-09-12 13:12:52 -0400

    update with new ask my pdf

--- Chat with CSV project/askmypdf.py
+++ Chat with CSV project/askmypdf.py
@@ -4,18 +4,13 @@ import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
 import requests
+import PyPDF2
+import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Initialize environment variables
 load_dotenv()
-
-# Check and store OpenAI API key
-if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-    print("OPENAI_API_KEY is not set")
-    exit(1)
-else:
-    print("OPENAI_API_KEY is set")
 
 # Initialize session state variables if they don't exist
 if 'df' not in st.session_state:
@@ -24,23 +19,15 @@ if 'df' not in st.session_state:
 if 'prompt_history' not in st.session_state:
     st.session_state.prompt_history = []
 
+# Check and store OpenAI API key
+if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+    print("OPENAI_API_KEY is not set")
+    exit(1)
+else:
+    print("OPENAI_API_KEY is set")
+
 # Store API token in session state
 st.session_state.openai_key = os.getenv("OPENAI_API_KEY")
-
-# Function to find the most relevant rows based on the question
-def find_relevant_rows(question, df, num_rows=1):
-    documents = [question] + list(df.to_string(header=False, index=False))
-    vectorizer = TfidfVectorizer().fit_transform(documents)
-    cosine_matrix = cosine_similarity(vectorizer[0:1], vectorizer[1:])
-    most_similar_row_index = cosine_matrix.argsort()[0][-num_rows:][::-1]
-    
-    # Check if we have any similar rows, else use the entire DataFrame
-    if most_similar_row_index.size > 0:
-        most_similar_rows = df.iloc[most_similar_row_index]
-    else:
-        most_similar_rows = df
-    
-    return most_similar_rows
 
 # Function to ask the OpenAI chat model
 def ask_openai_chat_model(question, context):
@@ -59,18 +46,33 @@ def ask_openai_chat_model(question, context):
     response = requests.post(url, json=payload, headers=headers)
     return response.json()['choices'][0]['message']['content']
 
+# Function to find the most relevant paragraph based on the question
+def find_relevant_paragraph(question, text, num_paragraphs=1):
+    paragraphs = re.split('\n\n+', text)
+    documents = [question] + paragraphs
+    vectorizer = TfidfVectorizer().fit_transform(documents)
+    cosine_matrix = cosine_similarity(vectorizer[0:1], vectorizer[1:])
+    most_similar_paragraph_index = cosine_matrix.argsort()[0][-num_paragraphs:][::-1]
+    most_similar_paragraphs = " ".join([paragraphs[i] for i in most_similar_paragraph_index])
+    return most_similar_paragraphs
+
 # Streamlit Interface
-st.title("Ask your CSV 📈")
+st.title("Ask My PDF 💾")
+st.write("Upload a PDF and ask it questions.")
 
 # File Uploader
 if st.session_state.df is None:
-    uploaded_file = st.file_uploader("Choose a CSV file.", type="csv")
+    uploaded_file = st.file_uploader("Choose a PDF file.", type=["pdf"])
     if uploaded_file is not None:
         try:
-            df = pd.read_csv(uploaded_file)
-            st.session_state.df = df
+            pdf_reader = PyPDF2.PdfReader(uploaded_file)
+            text = ""
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+            st.session_state.df = pd.DataFrame({'Text': text.split('\n')})
         except Exception as e:
-            st.error(f"Error reading CSV: {e}")
+            st.error(f"Error reading PDF: {e}")
 
 # Form for Question
 with st.form("Question"):
@@ -79,17 +81,18 @@ with st.form("Question"):
     if submitted:
         try:
             with st.spinner():
-                relevant_rows = find_relevant_rows(question, st.session_state.df)
-                answer = ask_openai_chat_model(question, relevant_rows.to_string(header=False, index=False))
+                extracted_text = " ".join(st.session_state.df['Text'].dropna())
+                relevant_paragraph = find_relevant_paragraph(question, extracted_text)
+                answer = ask_openai_chat_model(question, relevant_paragraph)
                 st.write(answer)
                 st.session_state.prompt_history.append(question)
         except Exception as e:
             st.error(f"Error processing question: {e}")
 
 # Display current DataFrame and prompt history
-if st.session_state.df is not None:
-    st.subheader("Current dataframe:")
-    st.write(st.session_state.df)
+# if st.session_state.df is not None:
+#     st.subheader("Current dataframe:")
+#     st.write(st.session_state.df)
 
 st.subheader("Prompt history:")
 st.write(st.session_state.prompt_history)
